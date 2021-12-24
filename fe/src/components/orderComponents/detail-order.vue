a9ede4fb81c670452599699f907af07ebcb839b3
Cao Trọng Nghĩa <thekrualfox@gmail.com>
2021-12-24 11:52:31 +0700

    order detail fixed

--- fe/src/components/orderComponents/detail-order.vue
+++ fe/src/components/orderComponents/detail-order.vue
@@ -1,47 +1,39 @@
 <template>
   <div class="order">
     <v-card outlined>
-      <v-card-title>Đơn hàng {{ datas.id }}</v-card-title>
-      <v-card-text>
-        <v-list>
+      <v-card-title>Mã đơn hàng {{ datas.id }}</v-card-title>
+      <v-card-text class="pb-0">
+        <v-list two-line>
           <v-list-item v-for="item in datas.details" :key="item.perfume">
             <v-list-item-avatar>
               <v-img
                 :src="item.perfume_data.image"
-                max-height="50px"
                 max-width="50px"
                 contain
                 :alt="item.perfume_data.name"
               />
             </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>{{
+                item.perfume_data.name
+              }}</v-list-item-title>
+              <v-list-item-subtitle>x{{ item.amount }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action-text>
+              {{ (item.price * item.amount) | toCurrency }}
+            </v-list-item-action-text>
           </v-list-item>
         </v-list>
       </v-card-text>
-    </v-card>
-    <div v-for="item in datas.details" :key="item.perfume" class="list-item">
-      <div class="img">
-        <div class="left">
-          <v-img
-            :src="item.perfume_data.image"
-            max-width="70px"
-            max-heght="70px"
-            :alt="item.perfume_data.name"
-          />
-          <div class="item-detail">
-            <h4 style="color: white">{{ item.perfume_data.name }}</h4>
-            <p style="color: white">x{{ item.amount }}</p>
+      <v-card-actions>
+        <v-container fluid class="d-flex myContainer">
+          <div class="col1">Tổng số tiền:</div>
+          <div class="col2">
+            {{ datas.totalPrice | toCurrency }}
           </div>
-        </div>
-        <div class="p">
-          <p style="color: white">{{ item.perfume_data.price | toCurrency }}</p>
-        </div>
-      </div>
-      <div class="price">
-        <h3>
-          Tổng tiền: {{ (item.amount * item.perfume_data.price) | toCurrency }}
-        </h3>
-      </div>
-    </div>
+        </v-container>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -59,9 +51,7 @@ export default {
     getOrderDetail() {
       NHDataServices.getOrderByID(this.id).then((res) => {
         this.datas = res.data;
-        this.loader = false;
-
-        console.log(this.datas);
+        this.loading = false;
       });
     },
   },
@@ -89,71 +79,23 @@ export default {
   max-width: 1200px;
   margin: 2em auto;
 }
-
-.img {
-  padding: 1em;
-  background-color: #009688;
-  display: flex;
-  justify-content: space-between;
-  box-shadow: rgba(230, 224, 224, 0.1) 0px 4px 12px;
-  border-bottom: 1px solid #ffffff36;
-  border-radius: 5px;
+.order .v-card__title {
+  /* border-bottom: 1px dashed rgba(0, 0, 0, 0.3); */
+  background-color: #009688 !important;
+  color: #fff;
 }
-
-.order .img img {
-  width: 50px;
-  height: 50px;
+.order .v-card__actions {
+  border-top: 1px dashed rgba(0, 0, 0, 0.3);
 }
-
-.left {
-  display: flex;
-  align-items: center;
+.order .v-list-item {
+  border-bottom: 1px dotted rgba(0, 0, 0, 0.2);
 }
-
-.item-detail > h4,
-p {
-  margin-left: 1em;
-  font-size: 15px;
+.order .v-list-item:last-child {
+  border-bottom: none;
 }
-
-.p {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.item-detail {
-  display: flex;
-  flex-direction: column;
-  align-content: flex-start;
-}
-.price {
-  display: flex;
+.order .myContainer {
+  gap: 2rem;
+  align-content: center;
   justify-content: flex-end;
-  align-items: center;
-  padding: 1em;
-  background-color: white;
-  border-radius: 5px;
-}
-
-.footer {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  background-color: #08080861;
-  padding: 1em;
-}
-
-.header {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  background-color: #08080861;
-  padding: 1em;
-}
-
-.list-item {
-  margin: 2em 0;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
 }
 </style>
